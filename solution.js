1889a219b372df66870ed1a012dd87e7f1d4b146
Shazammon <ammonorgill@gmail.com>
2022-12-16 12:18:29 -0700

    updated instructions reading code

--- solution.js
+++ solution.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const { parse } = require('path')
 
 
-let data = fs.readFileSync('instructions.txt', 'utf-8')
+let data = fs.readFileSync('data.txt', 'utf-8')
 
 const stringData = data.split(/\r?\n/)
