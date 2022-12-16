4d8eef2260e04b28be8fdf11ae75cff454b00ce8
Shazammon <ammonorgill@gmail.com>
2022-12-16 12:19:10 -0700

    began writing pseudo code

--- solution.js
+++ solution.js
@@ -5,3 +5,6 @@ const { parse } = require('path')
 let data = fs.readFileSync('data.txt', 'utf-8')
 
 const stringData = data.split(/\r?\n/)
+
+// how can i do this
+// need to write some specific code
