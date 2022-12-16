d9556bc71e53be02eb40bf7f072391d002260618
Shazammon <ammonorgill@gmail.com>
2022-12-16 12:20:13 -0700

    set up variable

--- solution.js
+++ solution.js
@@ -9,6 +9,7 @@ const stringData = data.split(/\r?\n/)
 // how can i do this
 // need to write some specific code
 
+let variable = 0
 // need to figure out the function
 function readCode() {
     return variable
