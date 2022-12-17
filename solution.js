75089ef61be9a7b7545390a8b21e8545bada7e5c
Shazammon <ammonorgill@gmail.com>
2022-12-17 16:55:15 -0700

    cleaned up code, removed console.logs

--- solution.js
+++ solution.js
@@ -6,8 +6,6 @@ let data = fs.readFileSync('data.txt', 'utf-8')
 
 const stringData = data.split(/\r?\n/)
 
-// how can i do this
-// need to write some specific code
 
 let variable = 0
 // need to figure out the function
